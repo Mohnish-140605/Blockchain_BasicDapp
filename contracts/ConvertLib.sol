533a249855921024853f000793cfd3821c654dd8
Mohnish <mohnishg.bsc23@rvu.edu.in>
2025-03-28 11:22:12 +0530

    this is second commit to the repo

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,9 +1,8 @@
-//SPDX-License-Identifier: MIT
-pragma SOlidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-library ConvertLib{
-    function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
-    {
+library ConvertLib {
+    function convert(uint amount, uint conversionRate) public pure returns (uint convertedAmount) {
         return amount * conversionRate;
     }
 }
